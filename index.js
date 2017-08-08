e051d68cb08bbc95c1bb70b2b8b29dc3f6f4202a
Leonard <szx961102@gmail.com>
2017-08-09 00:53:24 +0800

    a

--- index.js
+++ index.js
@@ -7,11 +7,67 @@ let app = express();
 app.use(Bodyparser.urlencoded({extended:true}));
 app.use(cookieParser());
 
-let connection = mysql.createConnection({
-  host     : '119.28.63.95',
-  user     : 'myuser',
-  password : 'hubuedu',
-  database : 'twsjob'
+var connection = mysql.createConnection({
+  host: '119.28.63.95',
+  user: 'myuser',
+  password: 'hubuedu',
+  port: '3306',
+  database: 'twsjob',
 });
 
 connection.connect();
+
+// ## 7 用户查看自己创建的职位Post列表
+// 作为已注册并登陆的用户（招聘者），我想浏览自己发布的所有工作 以便查看自己手上的所有招聘。
+app.get('/myposts', function (req, res) {
+  //得到用户的id
+  let userid = req.query.userid;
+  //查找用户的post
+  let sql = 'select * from t_job where userid = ' + userid
+  console.log(sql)
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      return;
+    }
+
+    console.log('--------------------------SELECT----------------------------');
+    console.log(result);
+    console.log('------------------------------------------------------------\n\n');
+    //返回自己全部的post
+    res.send(result)
+  })
+  connection.end();
+})
+
+// ## 8 用户查看自己创建的职位Post详情
+// 作为已注册并登陆的用户（招聘者)，我想浏览自己发布的某一个招聘工作的详细信息 以便知道该招聘的详细信息。
+app.get('/post', function (req, res) {
+  if (err) {
+    console.log('[SELECT ERROR] - ', err.message);
+    return;
+  }
+  //得到工作的id
+  let id = req.query.id;
+  let sql = 'select * from t_job where id = ' + id
+  console.log(sql)
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      return;
+    }
+
+    console.log('--------------------------SELECT----------------------------');
+    console.log(result);
+    console.log('------------------------------------------------------------\n\n');
+    //返回自己全部的post
+    res.send(result)
+  })
+  connection.end();
+})
+
+
+app.listen(8081, function () {
+  console.log('listening!')
+})
+
