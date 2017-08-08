fc9e92a26730d621ba88a20b975093155f328410
童天相 <Heikunan@users.noreply.github.com>
2017-08-08 15:46:17 +0800

    final

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let express = require('express');
 let Bodyparser = require('body-parser');
 let cookieParser = require('cookie-parser');
+let session = require('express-session');
 let mysql = require('mysql');
 
 let app = express();
 app.use(cookieParser());
+
+let connection = mysql.createConnection({
+  host     : '119.28.63.95',
+  user     : 'myuser',
+  password : 'hubuedu',
+  database : 'twsjob'
+});
