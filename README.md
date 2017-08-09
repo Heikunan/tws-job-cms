4dab4a95c4322d144b5662fb87921f227a0afb4f
ysj <2534123987@qq.com>
2017-08-09 17:20:53 +0800

    邮箱验证

--- README.md
+++ README.md
@@ -28,11 +28,166 @@
 
 [nodeJS操作mysql文档地址](http://www.runoob.com/nodejs/nodejs-mysql.html)
 
+# 关于express-session和cookie
+
+### 使用cookie与session技术使用户可以跨页面保持登录状态
+
+```
+let express = require('express');
+let cookieParser = require('cookie-parser');
+let session = require('express-session');
+//首先引入相关组件
+let app = express();
+//使用session与cookie组件
+/*
+    express-session和cookie-parser这两个组件联合使用可以在用户第一次访问的时候在用户浏览器里面写入cookie
+*/
+app.use(cookieParser('twsjob'));
+app.use(session({
+  name:'twsjob',//设置写入用户浏览器cookie的key
+  secret: 'twsjob',//签名，与cookie保持一致
+  resave: true,
+  maxAge: 90000,//设置失效时间单位为ms
+  saveUninitialized: true
+}));
+//访问代码
+app.get('/', function (res, req) {
+  if (res.session.visit) {
+    res.session.visit++;
+    req.json(res.session);
+  } else {
+    res.session.visit = 1;
+    req.send("第一次访问页面");
+  }
+});
+
+app.get('/getsession',function(res,req){
+  /*
+    每次用户访问服务器就会自动在http请求头里面带上本地的cookie，但是获取用户本地的cookie的时候一定要使用res.session.你的cookie的key,在以上代码里，第一次访问localhost:3000的时候会输出第一次访问页面，以后每次访问res.session.visit都会加一
+  */
+  /*
+  这是得到session，在服务器端保存着的是一个object对象
+
+  {
+    "cookie": {
+        "originalMaxAge": null,
+        "expires": null,
+        "httpOnly": true,
+        "path": "/"
+    },
+    "visit": 18
+   }   
+
+  */
+  let mysession = res.session.twsjob;
+  console.log(res.session.twsjob);
+})
+
+app.listen(3000);
+/*
+    当浏览器访问localhost:3000的时候,初始就会自动写入一个cookie而且key为twsjob,本地cookie储存的是session的key，通过res.session.你的cookie的key可以获取你设置session的值
+*/
+
+/*
+    当我访问localhost:3000/getsession的时候就可以获取我写入的twsjob这个session，可以同时写入多个session，当页面关闭的时候不会消失，只有浏览器关闭的时候session与cookie都会自动消失，可以通过这个技术来使用户可以跨页面登录
+*/
+```
+
+## 总结
+
+>在用户通过url访问我们的网站的时候,我们就会在用户的cookie里面自动生成一个我们定义好的key的sessionID（value），在用户在通过url访问我们的网站的时候，就会在http请求里携带上cookie，我们可以通过用户发送过来的cookie来找到相应的session，从而来管理用户在我们网站的各种状态
+
+## 后台API规范
+
+### GET 为后台为前台发送数据（数据获取）
+
+#### 代码示例
+
+```
+app.get('/somewhere',function(req,res)){
+     //dosomething...
+    res.send('Hello');
+}
+```
+
+### POST 为后台接收前台发送数据并发送结果（数据添加）
+
+#### 代码示例
+
+```
+app.post('/somewhere',function(req,res)){
+     //dosomething...
+    res.send('Hello');
+}
+```
+
+### PUT 为前台给后台发送数据（数据修改）
+
+#### 代码示例
+
+```
+app.put('/somewhere',function(req,res)){
+     //dosomething...
+    res.send('Hello');
+}
+```
+
+### DELETE 为前台给后台发送数据（删除）
+
+#### 代码示例
+
+```
+app.delete('/somewhere',function(req,res)){
+    //dosomething...
+    res.send('Hello');
+}
+```
+
+
+
+
+
 # 数据库的设计
 
-## 数据库的名称
+## 数据库的名称 twsjob
 
-### twsjob
+### 用户表 T_user
+
+|ID|用户主键|
+|---|---|
+|email|用户邮箱|
+|password|用户密码|
+|company|用户所在公司|
+|address|公司地址|
+|trade|公司所属行业|
+
+### 职位表 T_job
+
+|ID|职位主键|
+|---|---|
+|userId|用户ID|
+|title|用户密码|
+|company|用户所在公司|
+|description|公司地址|
+|applyApproach|公司所属行业|
+|expiryDate|职位申请的截止时间|
+|category|工作职位的种类|
+|jobType|工作性质|
+|tags|职位标签|
+|city|所在城市|
+|country|所在国家|
+
+### 工作性质 T_jobtype
+
+|ID|工作性质ID主键|
+|---|---|
+|content|工作性质的内容|
+
+### 职位种类 T_category
+
+|ID|职位种类ID主键|
+|---|---|
+|content|职位种类的名称|
 
 
 
--- index.js
+++ index.js
@@ -3,22 +3,35 @@ let Bodyparser = require('body-parser');
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
 let mysql = require('mysql');
+let nodemailer  = require('nodemailer');
+let Crypto = require('node-crypto');
 let urlencodedParser = Bodyparser.urlencoded({ extended: true });
+let cp = new Crypto('you secret code');
 
-var i=0;
 let app = express();
+app.use(Bodyparser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(session({
     secret: 'recommand 128 bytes random string', // 建议使用 128 个字符的随机字符串
     cookie: { maxAge: 60 * 1000 }
 }));
 
-
+/*连接数据库*/
 let connection = mysql.createConnection({
   host     : '119.28.63.95',
   user     : 'myuser',
   password : 'hubuedu',
   database : 'twsjob'
+});
+/*连接发送邮件的邮箱*/
+let mailTransport = nodemailer.createTransport({
+    host : 'smtp.126.com',
+    port: 25,
+    secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
+    auth : {
+        user : 'thoughtworkersfive@126.com',
+        pass : 'dalaodaifei555'
+    },
 });
 
 connection.connect();
@@ -40,47 +53,75 @@ app.post('/',urlencodedParser,function (req,res) {
 
 /*将用户登陆的数据传入*/
 app.get('/login',urlencodedParser, function (req, res) {
-    let username='888'//req.body.username;
-    let password='22'//req.body.password;
-    let sql='select * from t_user where email=? and password=?';
-    let sqlinfor=[username,password];
+    let username=req.body.username;     //req.body.username;
+    let password=req.body.password;     //req.body.password;
+    let sql='select * from t_user where email=? and password=?';        //sql查询语句
+    let sqlinfor=[username,password];               //sql问号的值
     connection.query(sql,sqlinfor,function (err, result) {
         if(err) throw  err;
-        console.log(result);
-        if(result.length===0){       //不存在这个用户,返回0;
-          res.send(result);
+        if(result.length===0){
+          res.send(false);          //不存在这个用户,返回false;
         }
-        else{       /*返回产生的session字符串*/
-            result[0].useridd=i;
-            i++;
-            req.session.user=result[0];
-             console.log(req.session);
-            res.send(result);      //返回产生的session,前台将其存入cookie中
+        else{
+            req.session.user=result[0];//将登陆的用户存入session
+            res.send(result[0]);        //返回查找结果，也是session 的用户
        }
     });
-
-});
-
-app.get('/login1',urlencodedParser, function (req, res) {
-    if(req.session.user){
-        console.log(req.session.user);
-      res.send('已登录！');
-    }else {
-      res.send('未登录！');
-    }
-});
-
-app.get('/login2',urlencodedParser, function (req, res) {
-    if(req.session.user){
-      req.session.user='';
-        res.send('登录并消除！'+req.session.user);
-    }else {
-        res.send("未登录");
-    }
 });
 
 //////////////////////////*杨邵军的测试*//////////////////////////////////////////
+/*发送邮件,将用户信息绑定在里面发送过去，并不完善,只有id,email,password,激活码的存放*/
+app.post('/send', function(req, res, next) {
+    /*得到前台的数据*/
+    let id=req.body.id;
+    let email=req.body.email;
+    let password=req.body.password;
+    //邮件中显示的信息
+    let html="欢迎注册请<a href='http://localhost:8081/confirm?hex="+cp.hex(email)+"'>点击此处</a>确认注册!";
+    //sql语句插入语句
+    let sql='insert into t_user (id,password,email,activeToken) values (?,?,?,?);';
+    /*数据库中存hex数据,除了激活码是email 的base数据*/
+    let sqlinfor=[parseInt(id),cp.hex(password),cp.hex(email),cp.base(email)];
+    connection.query(sql,sqlinfor,function (err,result) {
+        if(err) {
+            //插入失败，返回false，就是用户已经存在
+            res.send(false);
+        }else{
+            /*设置邮件信息,如果可以插入数据库中*/
+            let options = {
+                from           : 'ysj<thoughtworkersfive@126.com>',
+                to             :  email,
+                subject        : '注册成功，请激活！',
+                text           : '欢迎注册',
+                html           :  html
+            };
+            /*发送邮件*/
+            mailTransport.sendMail(options, function(err, msg){
+                if(err){
+                    console.log(err);
+                }
+                else {
+                    console.log(msg);
+                    res.send("发送成功！");
+                }
+            });
+        }
+    });
+});
 
+/*邮箱中点击此处确定，返回到这个界面，将邮箱激活*/
+app.get('/confirm', function(req, res, next) {
+    let url=req.query;
+    /*将传入的hex(email)转化为base ，在数据库中查询*/
+    let sql="update t_user set active=1 where activeToken='"+cp.hexToBase(url.hex)+"'";
+    connection.query(sql,function (err, result) {
+        if(err) throw  err;
+        else {          //如果没有直接对数据库进行操作，不会返回error
+            res.send("注册成功！");
+        }
+    });
+
+});
 
 let server = app.listen(8081, function () {
     let host = server.address().address;
