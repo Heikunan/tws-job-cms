59dd10c027cca51785ace33ad06c5c85d46ed78c
Heikunan <wasdwasdk88@qq.com>
2017-08-14 23:33:31 +0800

    123

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.use(Bodyparser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use(session({
+    name: 'twsjob',
     secret: 'recommand 128 bytes random string', // 建议使用 128 个字符的随机字符串
-    cookie: { maxAge: 60 * 1000 }
+    cookie: { maxAge: 600 * 1000 }
 }));
 
 let connection = mysql.createConnection({
