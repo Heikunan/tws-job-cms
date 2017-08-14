2e3a9b171a52908d7dad82da1036f39c04a0f8f4
Heikunan <wasdwasdk88@qq.com>
2017-08-14 17:07:13 +0800

    '13'

--- public/js/common.js
+++ public/js/common.js
@@ -1,9 +1,8 @@
-
-    $.get('/getUserInfo',function (ans) {
+ $.get('/getUserInfo',function (ans) {
         if(ans !== 'no'){
-            console.log(ans);
-            let str=`<li><a href="../jobPost.html">getposts</a></li>`;
+            let str = '';
             str += `<li><a href="../userInfo.html">${ans.email}</a></li>`;
+            str +=`<li><a href="../jobPost.html">getposts</a></li>`;
             $('.myheader-right').empty().append(str);
         }
     });
