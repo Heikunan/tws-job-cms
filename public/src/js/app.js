8b129b549b8f97512267e5686aedeba27d883373
Heikunan <wasdwasdk88@qq.com>
2017-08-15 11:42:20 +0800

    '123'

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,44 +1,37 @@
 $.post('/testjobs',{num:1},function (ans) {
     let str = '';
     for(let i = 0;i<ans.length;i++){
-        str += `<div class="myjob-contain">
-							<a href="./jobInfo.html?id=${ans[i].id}">
-							<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-								<div class="panel panel-default mybox">
-									<div class="panel-body">
-										<div class="img-box">
-											<img class="img-responsive"  src="http://szimg.mukewang.com/5987c96700019fba05400300-360-202.jpg">
-										</div>
-										<div class="job-box">
-											<div class="job-title">
-												<span>${ans[i].title}</span>
-											</div>
-											<div class="job-description">
-												<span>${ans[i].description}</span>
-											</div>
-											<div class="job-company">
-												<p><span class="glyphicon glyphicon-globe"></span>&nbsp;${ans[i].company}</p>
-											</div>
-											<div class="job-bottom">
-												<div class="pull-left">
-													<div class="job-city">
-														<p><span class="glyphicon glyphicon-map-marker"></span>&nbsp;${ans[i].country}</p>
-													</div>
-												</div>
-												<div class="pull-right job-bottom-right">
-													<div class="job-time">
-														<p><span class="glyphicon glyphicon-time"></span>&nbsp;${ans[i].expiryDate}</p>
-													</div>
-												</div>
-											</div>
-										</div>
+        str+=`<div class="panel panel-default">
+					<a href="">
+						<div class="panel-body">
+							<div class="job-card">
+								<div class="job-primary">
+								<div class="info-primary">
+									<h3 class="name">技术总监 <span class="red">10K-15K</span></h3>
+									<p>武汉<em class="vline"></em>5-10年<em class="vline"></em>大专</p>
+								</div>
+								<div class="info-company">
+									<div class="company-text">
+										<h3 class="name">兆讯移动</h3>
+										<p>移动互联网<em class="vline"></em>不需要融资<em class="vline"></em>20-99人</p>
 									</div>
 								</div>
 							</div>
-							</a>
-						</div>`;
+								<div class="job-tags">
+								<div class="job-author">
+									<p>潘荣荣<em class="vline"></em>行政人力主管<img src="https://img.bosszhipin.com/beijin/mcs/useravatar/20161013/2438e95364d793dd5f0edc6f6b4f08718c7dd922ad47494fc02c388e12c00eac_s.jpg"></p>
+								</div>
+								<span>系统架构</span><span>高级技术管理</span><span>APP开发</span>
+							</div>
+								<div class="job-time">
+								<div class="time">发布于06月06日</div>
+							</div>
+							</div>
+						</div>
+					</a>
+				</div>`;
     }
-    $('#myjobcontain').empty().append(str);
+    $('#myleft-job-all').empty().append(str);
 });
 
 $(document).ready(function () {
