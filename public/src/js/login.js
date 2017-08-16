bbaa097ea757e035a731c33230ad506c2f0f611e
Cuijiya <2401932820@qq.com>
2017-08-16 12:05:48 +0800

    alert

---
+++ public/src/js/login.js
@@ -0,0 +1,30 @@
+$(function () {
+    //点击登陆按钮，进行登陆操作
+    $('#login').click(function () {
+        let email=$('#email').val();
+        let password=$('#password').val();
+        $.post('/login',{
+            email:email,password:password
+        },function (data) {
+            if (data==='ok') {
+                window.location.assign('/');
+            }else if(data==='wrong'){
+                $('.flash_container').empty();
+                $('.flash_container').append('<div class="alert">密码错误！</div>')
+                hide();
+            } else if (data==='inactivated') {
+                $('.flash_container').empty();
+                $('.flash_container').append('<div class="alert">您已注册，请前往邮箱激活账号！</div>')
+                hide();
+            }else if(data==='null'){
+                $('.flash_container').empty();
+                $('.flash_container').append('<div class=alert>账号不存在，请注册后登陆！</div>')
+                hide();
+            }
+        });
+    });
+});
+
+function hide() {
+    $('.alert').delay(1000).hide(0);
+    }
