42bc36aa237d70fc3edb8746d5082639c6cbab46
Jiya <Cuijiya>
2017-09-22 22:13:34 +0800

    repair

--- public/src/js/no_email.js
+++ public/src/js/no_email.js
@@ -7,9 +7,14 @@ $(function () {
         },function (data) {
             if (data===true) {
                 $('.flash_container').empty();
-                $('.flash_container').append('<div class=alert>邮件已发送，请前往邮箱验证！</div>');
-                hide();
-            }else {
+                $('.flash_container').append('<div class=alert>邮件已发送，请前往邮箱验证！5秒后页面将跳转至首页！！</div>');
+                window.setTimeout("window.location='/'",5000);
+            }else if(data==='wrong'){
+                $('.flash_container').empty();
+                $('.flash_container').append('<div class=alert>账号已注册，请前往首页登陆。5秒后页面将跳转至首页！！</div>');
+                window.setTimeout("window.location='/'",5000);
+            }
+            else{
                 $('.flash_container').empty();
                 $('.flash_container').append('<div class=alert>该账号尚未注册，请注册！</div>');
                 hide();
